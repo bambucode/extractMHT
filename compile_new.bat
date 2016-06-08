9461fdc48bf381d3ab369063fb0d04e0b8c1d277
Luis Carrasco <luis@bambucode.com>
2016-06-08 09:48:47 -0600

    Initial commit.

---
+++ compile_new.bat
@@ -0,0 +1,1 @@
+"Y:\windows\apps\CompileAU3.exe" /in extractMHT.au3 /out extractMHT.exe /comp 4 /icon ..\Support\Icons\extractmht_exe.ico /nodecompile /prod
---
+++ generate_noinst.bat
@@ -0,0 +1,2 @@
+del extractMHT10.rar
+winrar.exe a -m5 -s extractMHT10.rar ..\extractmht_changelog.txt ..\extractmht_license.txt ..\support\Icons *.*
